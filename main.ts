9393fc7a350fe6abba73cc39bc89159219731080
Kyle Alan Hale <kylealanhale@gmail.com>
2023-08-03 17:21:25 -0400

    add basic support for hiding annotation markup and adding decorator

--- main.ts
+++ main.ts
@@ -1,137 +1,135 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
+
+import { syntaxTree } from "@codemirror/language";
+import {
+    Extension,
+    RangeSetBuilder,
+    StateField,
+    Transaction,
+} from "@codemirror/state";
+import {
+    Decoration,
+    DecorationSet,
+    EditorView,
+    WidgetType,
+} from "@codemirror/view";
+
+
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface KAHFMSettings {
+    mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: KAHFMSettings = {
+    mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class KAHFM extends Plugin {
+    settings: KAHFMSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // This adds a settings tab so the user can configure various aspects of the plugin
+        this.addSettingTab(new KAHFMSettingsTab(this.app, this));
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        this.registerEditorExtension([annotationField])
+    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    onunload() {
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+export class AnnotationWidget extends WidgetType {
+    toDOM(view: EditorView): HTMLElement {
+        const el = document.createElement("span");
+        el.addClass('annotation-widget')
+        el.innerText = "{}";
+        return el;
+    }
 }
+export const annotationField = StateField.define<DecorationSet>({
+    create(state): DecorationSet {
+        return Decoration.none;
+    },
+    update(oldState: DecorationSet, transaction: Transaction): DecorationSet {
+        // console.log('oldState', oldState, "transaction", transaction);
+        const builder = new RangeSetBuilder<Decoration>();
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        syntaxTree(transaction.state).iterate({
+            enter(node: any) {
+                // console.log('node', node, node.type.name, node.from + 1, node.to + 1);
+                const nextCharIsBracket = transaction.state.doc.sliceString(node.to + 1, node.to + 2) == "{"
+                if (node.type.name == "hmd-barelink_link" && nextCharIsBracket) {
+                    const text = transaction.state.doc.sliceString(node.from, node.to)
+                    // Find where the next closing curly bracket is
+                    const closingBracketIndex = transaction.state.doc.sliceString(node.to + 1).indexOf("}") + node.to + 2
+                    builder.add(
+                        node.from - 1,
+                        node.from,
+                        Decoration.replace({})
+                    );
+                    builder.add(
+                        node.to,
+                        node.to + 2,
+                        Decoration.replace({})
+                    );
+                    builder.add(
+                        node.to + 2,
+                        closingBracketIndex,
+                        Decoration.replace({ 
+                            widget: new AnnotationWidget(),
+                        })
+                    );
+                }
+            },
+        });
+    
+        return builder.finish();
+    },
+    provide(field: StateField<DecorationSet>): Extension {
+        return EditorView.decorations.from(field);
+    },
+});
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+class KAHFMSettingsTab extends PluginSettingTab {
+    plugin: KAHFM;
 
-	display(): void {
-		const {containerEl} = this;
+    constructor(app: App, plugin: KAHFM) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-		containerEl.empty();
+    display(): void {
+        const {containerEl} = this;
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+
+        new Setting(containerEl)
+            .setName('Setting #1')
+            .setDesc('It\'s a secret')
+            .addText(text => text
+                .setPlaceholder('Enter your secret')
+                .setValue(this.plugin.settings.mySetting)
+                .onChange(async (value) => {
+                    console.log('Secret: ' + value);
+                    this.plugin.settings.mySetting = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
